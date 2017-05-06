6a96e41410485596d3adb1b049a2c5c37b4ed513
Brandon White <bmwqg@live.com>
2017-05-06 16:32:01 -0400

    updated DrivingResponses layout to mimic Google Maps settings

--- app/src/main/java/bz/bdu/autoreply/DrivingResponsesActivity.java
+++ app/src/main/java/bz/bdu/autoreply/DrivingResponsesActivity.java
@@ -1,5 +1,9 @@
 package bz.bdu.autoreply;
 
+import android.Manifest;
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,6 +17,7 @@ public class DrivingResponsesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driving_responses);
+
     }
 
 }
